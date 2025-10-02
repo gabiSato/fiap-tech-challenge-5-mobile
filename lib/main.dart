bf3fb489e1e2a571f135060954b9c39577c69cb0
Gabriela Sato <gabisato.ssb@gmail.com>
2025-10-02 20:03:53 -0300

    build: set firebase auth local persistence

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:fiap_farms/ui/core/themes/theme.dart';
@@ -9,6 +10,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final auth = FirebaseAuth.instanceFor(app: Firebase.app());
+  await auth.setPersistence(Persistence.LOCAL);
 
   runApp(const MyApp());
 }
