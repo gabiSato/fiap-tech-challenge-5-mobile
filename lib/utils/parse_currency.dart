b72e4d13e2dfb79bd8a0064a976878b421b3b703
Gabriela Sato <gabisato.ssb@gmail.com>
2025-10-09 15:53:26 -0300

    fix: currency parse

--- lib/utils/parse_currency.dart
+++ lib/utils/parse_currency.dart
@@ -1,6 +1,7 @@
 double? parseCurrency(String? formattedCurrency) {
   if (formattedCurrency == null) return null;
 
-  String numericcString = formattedCurrency.replaceAll(r'[^0-9]', '');
+  String numericcString = formattedCurrency.replaceAll(RegExp('[^0-9]'), '');
+
   return (double.tryParse(numericcString) ?? 0.0) / 100;
 }
